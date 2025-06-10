4e7da0efecdaebb4492a7f22d120341c5d73ebb4
Max <55395652+kontr0x@users.noreply.github.com>
2025-06-10 16:25:53 +0200

    feat(main): refactor command-line interface and update output handling chore(docker): adjust volume paths and working directory in docker-compose chore(gitignore): add entries for build binaries and output files

--- go.mod
+++ go.mod
@@ -1,5 +1,7 @@
-module github.com/coffeemakingtoaster/cv-gen
+module github.com/kontr0x/goCV
 
 go 1.24.3
 
 require gopkg.in/yaml.v3 v3.0.1
+
+require github.com/urfave/cli/v3 v3.3.3 // indirect
--- main.go
+++ main.go
@@ -1,34 +1,52 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"time"
 
-	"github.com/coffeemakingtoaster/cv-gen/pkg/content"
+	"github.com/kontr0x/goCV/pkg/content"
+	cli "github.com/urfave/cli/v3"
 )
 
-var defaultContentYamlPath = "./content.yaml"
-
 func main() {
-	contentPath := defaultContentYamlPath
-	if len(os.Args) > 1 {
-		contentPath = os.Args[1]
+	cmd := &cli.Command{
+		Name:  "goCV",
+		Usage: "CV generator using Go and LaTeX",
+		Arguments: []cli.Argument{
+			&cli.StringArg{
+				Name:  "content",
+				Value: "./content.yaml",
+			},
+		},
+		ArgsUsage: "content.yaml",
+		Flags: []cli.Flag{
+			&cli.StringFlag{Name: "output", Value: "./output", Usage: "Output directory for the rendered CV"},
+		},
+		Action: func(ctx context.Context, c *cli.Command) error {
+			data, err := content.ParseContentFromYaml(c.StringArg("content"))
+			t := time.Now().Format("02-01-2006_15:04")
+			if err != nil {
+				panic(err)
+			}
+			for _, entry := range data {
+				dir := fmt.Sprintf("%s/%s_%s", c.String("output"), entry.Content.Version, t)
+				err := content.RenderTemplate("", "", dir, entry)
+				if err != nil {
+					panic(err)
+				}
+				err = content.BuildTemplate(dir)
+				if err != nil {
+					panic(err)
+				}
+			}
+			return nil
+		},
 	}
-	fmt.Printf("Reading content from %s\n", contentPath)
-	data, err := content.ParseContentFromYaml(contentPath)
-	if err != nil {
-		panic(err)
-	}
-	for _, entry := range data {
-		dir := fmt.Sprintf("./%s-out", entry.Content.Version)
-		// TODO: Add command line flag parsing to allow for custom passed paths
-		err := content.RenderTemplate("", "", dir, entry)
-		if err != nil {
-			panic(err)
-		}
-		err = content.BuildTemplate(dir)
-		if err != nil {
-			panic(err)
-		}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
 	}
 }
